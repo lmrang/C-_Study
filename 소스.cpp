9b601184f717bddc48d7d7271edfa3227a0595a5
limlr@naver.com <limlr@naver.com>
2022-03-27 02:53:59 +0900

    수정 1

--- 소스.cpp
+++ 소스.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
 #include <cstring>
+#include<vector>
 using namespace std;
 
 class Product {
 private:
+	int num;
 	int price;
 	string description;
 public:
-	Product(int money, string des) : price(money)
+	Product(int idx, int money, string des) : num(idx), price(money)
 	{
 		description = des;
 	}
 
-	virtual void ShowData()
+	virtual void ShowData() const
 	{
-		cout << "가격 : " << price << endl;
+		cout << endl;
 		cout << "종류 : " << description << endl;
-	}
-	void getProduct()
-	{
-
+		cout << "가격 : " << price << endl;
 	}
 };
 /******************************************************************/
@@ -29,85 +28,42 @@ private:
 	string writer;
 	string publisher;
 public:
-	Book(string bt, string bw, string bp, int money, string des)
-	: Product(money, des)
+	Book(int idx,  string bt, string bw, string bp, int money, string des)
+	: Product(idx, money, des) 
 	{
 		title = bt;
 		writer = bw;
 		publisher = bp;
 	}
-	void ShowData()
+	void ShowData() const
 	{
+		Product::ShowData();
 		cout << "제목 : " << title << endl;
 		cout << "작가 : " << writer << endl;
-		cout << "출판사 : " << publisher << endl;
-		Product::ShowData();
-	}
-	void getProduct()
-	{
-
+		cout << "출판사 : " << publisher << endl << endl;
 	}
 };
 /*******************************************************************/
-class ProductHandler {
-private:
-	Product* pobj[100];
-	//int idx;
-public:
-	ProductHandler() {}
-	void AddProduct(Product* id, int idx)
-	{
-		cout << idx;
-		pobj[idx]=id;
-	}
-	void ShowData(int idx) {
-		for (int i = 0; i < idx; i++)
-		{
-			pobj[i]->ShowData();
-			
-		}
-	}
-};
-/******************************************************************/
-
-void Plus() {
-	//Product* pobj[100];
-	static int idx = 0;
-
-	ProductHandler Handler;
-
+int choose_num()
+{
 	int choose;
-	string title, writer, publisher, decription;
-	int price;
 
 	cout << "1. Book " << endl;
 	cout << "2. CD" << endl;
 	cout << "3. cellphone" << endl;
 	cin >> choose;
 
-	if (choose == 1)
-	{
-		decription = "Book";
-		cout << "종류 : " << decription << endl;
-		cout << "제목 : ";
-		cin >> title;
-		cout << "작가 : ";
-		cin >> writer;
-		cout << "출판사 : ";
-		cin >> publisher;
-		cout << "가격 : ";
-		cin >> price;
-		
-		Handler.AddProduct(new Book(title, writer, decription, price, decription), idx);
-		cout << endl << endl;
-		Handler.ShowData(idx);
-	}
-
-	idx++;
+	return choose;
 }
-/******************************************************************/
+/*******************************************************************/
+int main() {	
+	Product* pobj[100];
+	static int idx = 0;
 
-int main() {
+	vector<int> book;
+
+	string title, writer, publisher, decription;
+	int price;
 	int choose;
 	while (1) {
 		cout << " 1. 상품추가 " << endl;
@@ -115,9 +71,42 @@ int main() {
 		cout << " 3. 종    료 " << endl;
 		cin >> choose;
 
-		if (choose == 1) Plus();
-		else if (choose == 2);
-		else break;
+		if (choose == 1) {
+			choose = choose_num();
+
+			if (choose == 1)
+			{
+				decription = "Book";
+				cout << "종류 : " << decription << endl;
+				cout << "제목 : ";
+				cin >> title;
+				cout << "작가 : ";
+				cin >> writer;
+				cout << "출판사 : ";
+				cin >> publisher;
+				cout << "가격 : ";
+				cin >> price;
+
+				pobj[idx] = new Book(idx, title, writer, publisher, price, decription);
+				book.push_back(idx);
+				idx++;
+
+			}
+		}
+		else if (choose == 2) {
+			choose = choose_num();
+
+			if (choose == 1)
+			{
+				vector<int>::iterator iter;
+
+				for (iter = book.begin(); iter != book.end(); iter++)
+					pobj[*iter]->ShowData();
+			}
+
+		}
+		else if (choose == 3) break;
+		else cout << "다시 입력해 주세요." << endl;
 	}
 	return 0;
 }
